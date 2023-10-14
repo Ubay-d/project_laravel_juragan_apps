968fc3ae8f7f4fe4060a777b4f94dec49cf8f8e3
LINI2019 <ubay.masykur@gmail.com>
2023-10-15 03:46:57 +0700

    Profile admin crud

--- routes/web.php
+++ routes/web.php
@@ -33,14 +33,19 @@ Route::middleware('auth')->group(function () {
 
 require __DIR__.'/auth.php';
 
+/// Admin Group Middleware
+
 Route::middleware(['auth','role:admin'])->group(function(){
 
     Route::get('/admin/dashboard', [AdminController::class, 'AdminDashboard'])->name('admin.dashboard');
     Route::get('/admin/logout', [AdminController::class, 'AdminLogout'])->name('admin.logout');
+    Route::get('/admin/profile', [AdminController::class, 'AdminProfile'])->name('admin.profile');
+    Route::post('/admin/profile/store', [AdminController::class, 'AdminProfileStore'])->name('admin.profile.store');
 
 }); // End Group Admin Middleware
 
 
+/// User Group Middleware
 Route::middleware(['auth','role:user'])->group(function(){
 
     Route::get('/user/dashboard', [UserController::class, 'UserDashboard'])->name('user.dashboard');
